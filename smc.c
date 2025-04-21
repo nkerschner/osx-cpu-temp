34c0a552c6e0befb1d26c1f8b03ae181953a1bc2
nkerschner <natekerschner@revivn.com>
2025-04-21 11:58:52 -0700

    Update smc.c

--- smc.c
+++ smc.c
@@ -56,7 +56,7 @@ kern_return_t SMCOpen(void)
     io_object_t device;
 
     CFMutableDictionaryRef matchingDictionary = IOServiceMatching("AppleSMC");
-    result = IOServiceGetMatchingServices(kIOMasterPortDefault, matchingDictionary, &iterator);
+    result = IOServiceGetMatchingServices(kIOMainPortDefault, matchingDictionary, &iterator);
     if (result != kIOReturnSuccess) {
         printf("Error: IOServiceGetMatchingServices() = %08x\n", result);
         return 1;
